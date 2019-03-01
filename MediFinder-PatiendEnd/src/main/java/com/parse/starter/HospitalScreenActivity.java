6c90a73e4dd30bb7abdd4c1cac54ce61204280c9
RohithS98 <ee16b025@iittp.ac.in>
2019-03-02 00:03:20 +0530

    Initial commit

---
+++ MediFinder-PatiendEnd/src/main/java/com/parse/starter/HospitalScreenActivity.java
@@ -0,0 +1,53 @@
+package com.parse.starter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class HospitalScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_screen);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    public void updateInfo(View view){
+        Intent intent = new Intent(HospitalScreenActivity.this, SignUp2Activity.class);
+        startActivity(intent);
+    }
+
+    public void logOut(View view) {
+        ParseUser.logOutInBackground(new LogOutCallback() {
+            @Override
+            public void done(ParseException e) {
+                Intent intent = new Intent(HospitalScreenActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+//                finish();
+                Intent intent = new Intent(HospitalScreenActivity.this, Main2Activity.class);
+                startActivity(intent);
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+}
---
+++ MediFinder-PatiendEnd/src/main/java/com/parse/starter/hospitalDetails.java
@@ -0,0 +1,23 @@
+package com.parse.starter;
+
+public class hospitalDetails {
+    public String name;
+    public String distance;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDistance() {
+        return distance;
+    }
+
+    public void setDistance(String distance) {
+        this.distance = distance;
+    }
+
+}
